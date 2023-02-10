2c4b02e251b9e272ba02b267a4abeab0eed28fb7
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2023-02-10 17:06:11 -0300

    ....

--- src/app/components/finance/finance.component.ts
+++ src/app/components/finance/finance.component.ts
@@ -8,32 +8,58 @@ import { Router } from '@angular/router';
 })
 export class FinanceComponent implements OnInit {
 
+  master: boolean = false;
   public form = [
-    { id: "AC-001", datail: 'Matricula', isChecked: true, mount: 350000, expirationDate: "05/02/2023" },
-    { id: "AC-002", datail: 'Marzo', isChecked: false, mount: 350000, expirationDate: "05/03/2023" },
-    { id: "AC-003", datail: 'Abril', isChecked: false, mount: 350000, expirationDate: "05/04/2023" },
-    { id: "AC-004", datail: 'Mayo', isChecked: false, mount: 350000, expirationDate: "05/05/2023" },
-    { id: "AC-005", datail: 'Junio', isChecked: false, mount: 350000, expirationDate: "05/06/2023" },
-    { id: "AC-006", datail: 'Julio', isChecked: false, mount: 350000, expirationDate: "05/07/2023" },
-    { id: "AC-007", datail: 'Agosto', isChecked: false, mount: 350000, expirationDate: "05/08/2023" },
-    { id: "AC-008", datail: 'Septiembre', isChecked: false, mount: 350000, expirationDate: "05/09/2023" },
-    { id: "AC-009", datail: 'Octubre', isChecked: false, mount: 350000, expirationDate: "05/10/2023" },
-    { id: "AC-010", datail: 'Noviembre', isChecked: false, mount: 350000, expirationDate: "05/11/2023" },
-    { id: "AC-011", datail: 'Diciembre', isChecked: false, mount: 350000, expirationDate: "05/12/2023" },
+    { id: "AC-001", datail: 'Matricula', select: false, mount: 350000, expirationDate: "05/02/2023" },
+    { id: "AC-002", datail: 'Marzo', select: false, mount: 350000, expirationDate: "05/03/2023" },
+    { id: "AC-003", datail: 'Abril', select: false, mount: 350000, expirationDate: "05/04/2023" },
+    { id: "AC-004", datail: 'Mayo', select: false, mount: 350000, expirationDate: "05/05/2023" },
+    { id: "AC-005", datail: 'Junio', select: false, mount: 350000, expirationDate: "05/06/2023" },
+    { id: "AC-006", datail: 'Julio', select: false, mount: 350000, expirationDate: "05/07/2023" },
+    { id: "AC-007", datail: 'Agosto', select: false, mount: 350000, expirationDate: "05/08/2023" },
+    { id: "AC-008", datail: 'Septiembre', select: false, mount: 350000, expirationDate: "05/09/2023" },
+    { id: "AC-009", datail: 'Octubre', select: false, mount: 350000, expirationDate: "05/10/2023" },
+    { id: "AC-010", datail: 'Noviembre', select: false, mount: 350000, expirationDate: "05/11/2023" },
+    { id: "AC-011", datail: 'Diciembre', select: false, mount: 350000, expirationDate: "05/12/2023" },
   ];
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
     //service bd
 
   }
 
-  ngOnInit() {}
-
-
-  goPagar(){
+  ngOnInit() {
+    
+  }
+  
+  
+  goPagar() {
     this.router.navigate(['/tbk'])
   }
+  
 
-    
+  onChange($event: any) {
+    const id = $event.detail.value;
+    const isChecked = $event.detail.checked;
+    console.log(id, isChecked);
 
+
+    this.form = this.form.map((d) => {
+      if (d.id == id) {
+        d.select = isChecked;
+        return d;
+      }
+      if (id == -1) {
+        d.select = this.master;
+        return d;
+      } else {
+        this.master = this.form.every((d) => d.select);
+        return d;
+      }
+    });
+    console.log(this.form);
+
+  }
 }
+
+//this.form.every((d) => d.select);
