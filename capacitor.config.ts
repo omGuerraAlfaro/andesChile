50539efefda8b135192e3a9cfd3b7430ce13d6f6
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2023-11-13 23:30:38 -0300

    ...

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'colegio.andes.chile.app',
   appName: 'andesChile',
   webDir: 'www',
   bundledWebRuntime: false
