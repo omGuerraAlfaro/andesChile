eecb82c108d1f6a56f21825e2183e25ed0cc8760
Omar Guerra Alfaro Mac <omarrrrignacio@gmail.com>
2024-04-13 10:24:39 -0400

    nose

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,22 @@
-package io.ionic.starter;
+package colegio.andes.chile.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+    }});
+
+  }
+}
